f3af9496a43c51fdbb26ef085160a38b51faaeea
Meron Fissha <meron.fissha@delta.com>
2020-12-10 20:59:56 -0500

    Added loging, code clean-up and userprofile endpoint

---
+++ src/main/java/com/solutions/datamart/dto/UserProfileResponse.java
@@ -0,0 +1,16 @@
+package com.solutions.datamart.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserProfileResponse {
+    private String message;
+    private int status;
+
+}
