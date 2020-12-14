93f6e9b9fc74ec064e8ed52fe8baeef65afb6c95
Meron Fissha <meron.fissha@delta.com>
2020-12-14 00:27:02 -0500

    Resolve merge conflict

--- src/main/java/com/solutions/datamart/controller/FaceBookPostController.java
+++ src/main/java/com/solutions/datamart/controller/FaceBookPostController.java
@@ -3,28 +3,15 @@ package com.solutions.datamart.controller;
 import com.solutions.datamart.model.FaceBookPost;
 import com.solutions.datamart.service.FacebookService;
 import lombok.extern.slf4j.Slf4j;
-import static com.solutions.datamart.util.Constants.EXCEPTION_MESSAGE;
-
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.solutions.datamart.model.FaceBookPost;
-import com.solutions.datamart.service.FacebookService;
 import java.util.Date;
 import java.util.List;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 import static com.solutions.datamart.util.Constants.EXCEPTION_MESSAGE;
 import static com.solutions.datamart.util.DateUtil.convertToDate;
 
@@ -41,15 +28,14 @@ public class FaceBookPostController {
             return facebookService.getAllLatestFaceBookPosts();
 
         } catch (Exception e) {
-			log.error(EXCEPTION_MESSAGE, "fetching data from database ", "allFBPosts", e.getMessage(), e.getCause());
+            log.error(EXCEPTION_MESSAGE, "fetching data from database ", "allFBPosts", e.getMessage(), e.getCause());
         }
         return null;
     }
 
     @GetMapping("/searchallfacebookpost")
-	public List<FaceBookPost> getAllFaceBookEntities(@RequestParam(required = false) String content, @RequestParam(required = false) String fromdate,@RequestParam(required = false) String todate) {
-		try 
-		{
+    public List<FaceBookPost> getAllFaceBookEntities(@RequestParam(required = false) String content, @RequestParam(required = false) String fromdate, @RequestParam(required = false) String todate) {
+        try {
             Date toDate = StringUtils.isEmpty(todate) ? new Date() : convertToDate(todate);
 
             if (!StringUtils.isEmpty(content) && !StringUtils.isEmpty(fromdate) && !StringUtils.isEmpty(todate)) {
@@ -63,10 +49,10 @@ public class FaceBookPostController {
             }
 
         } catch (Exception e) {
-			log.error(EXCEPTION_MESSAGE, "fetching facebook data from database ","getAllFaceBookEntities", e.getMessage(), e.getCause());
+            log.error(EXCEPTION_MESSAGE, "fetching facebook data from database ", "getAllFaceBookEntities", e.getMessage(), e.getCause());
 
-		}
         }
+
         return null;
     }
 
--- src/main/java/com/solutions/datamart/service/impl/RecordServiceImpl.java
+++ src/main/java/com/solutions/datamart/service/impl/RecordServiceImpl.java
@@ -1,18 +1,5 @@
 package com.solutions.datamart.service.impl;
 
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Iterator;
-import java.util.List;
-
-import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.time.DateUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.rometools.rome.feed.synd.SyndEntry;
 import com.rometools.rome.feed.synd.SyndFeed;
 import com.rometools.rome.io.SyndFeedInput;
@@ -22,98 +9,110 @@ import com.solutions.datamart.model.Record;
 import com.solutions.datamart.repository.RecordRepository;
 import com.solutions.datamart.service.MediaService;
 import com.solutions.datamart.service.RecordService;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.time.DateUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
 
 @Service("recordService")
 @Transactional
 public class RecordServiceImpl implements RecordService {
-	@Autowired
-	private RecordRepository recordRepository;
+    @Autowired
+    private RecordRepository recordRepository;
 
-	@Autowired
-	private MediaService mediaService;
+    @Autowired
+    private MediaService mediaService;
 
-	public void getAllMedias() {
-		List<Media> mediaList = mediaService.getAllMedias();
+    public void getAllMedias() {
+        List<Media> mediaList = mediaService.getAllMedias();
 
-		try {
-			for (Media media : mediaList) {
+        try {
+            for (Media media : mediaList) {
 
-				System.err.println(media.getName() + " Crawling started");
+                System.err.println(media.getName() + " Crawling started");
 
-				createRecord(media);
-			}
+                createRecord(media);
+            }
 
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		}
-	}
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public void createRecord(Media media) {
+    public void createRecord(Media media) {
 
-		Record record = null;
-		try {
-			URL url = new URL(media.getUrl());
-			HttpURLConnection httpcon = (HttpURLConnection) url.openConnection();
-			// Reading the feed
-			SyndFeedInput input = new SyndFeedInput();
-			SyndFeed feed = input.build(new XmlReader(httpcon));
+        Record record = null;
+        try {
+            URL url = new URL(media.getUrl());
+            HttpURLConnection httpcon = (HttpURLConnection) url.openConnection();
+            // Reading the feed
+            SyndFeedInput input = new SyndFeedInput();
+            SyndFeed feed = input.build(new XmlReader(httpcon));
 
-			List<SyndEntry> entries = feed.getEntries();
-			Iterator<SyndEntry> itEntries = entries.iterator();
+            List<SyndEntry> entries = feed.getEntries();
+            Iterator<SyndEntry> itEntries = entries.iterator();
 
-			List<Record> records = new ArrayList<>();
+            List<Record> records = new ArrayList<>();
 
-			Date date = new Date();
+            Date date = new Date();
 
-			while (itEntries.hasNext()) {
-				SyndEntry entry = itEntries.next();
+            while (itEntries.hasNext()) {
+                SyndEntry entry = itEntries.next();
 
-				if (DateUtils.isSameDay(entry.getPublishedDate(), date)
-						|| DateUtils.isSameDay(entry.getUpdatedDate(), date)) {
-					if (StringUtils.containsIgnoreCase(entry.getTitle(), "tigray")
-							|| StringUtils.containsIgnoreCase(entry.getDescription().getValue(), "tigray")) {
-						record = new Record();
-						record.setTitle(entry.getTitle());
-						record.setLink(entry.getLink());
-						record.setDescription(entry.getDescription().toString());
-						record.setCreatedDate(entry.getPublishedDate());
-						record.setUpdatedDate(entry.getUpdatedDate());
+                if (DateUtils.isSameDay(entry.getPublishedDate(), date)
+                        || DateUtils.isSameDay(entry.getUpdatedDate(), date)) {
+                    if (StringUtils.containsIgnoreCase(entry.getTitle(), "tigray")
+                            || StringUtils.containsIgnoreCase(entry.getDescription().getValue(), "tigray")) {
+                        record = new Record();
+                        record.setTitle(entry.getTitle());
+                        record.setLink(entry.getLink());
+                        record.setDescription(entry.getDescription().toString());
+                        record.setCreatedDate(entry.getPublishedDate());
+                        record.setUpdatedDate(entry.getUpdatedDate());
 
-						record.setMedia(media);
+                        record.setMedia(media);
 
-						records.add(record);
-					}
-				}
-			}
+                        records.add(record);
+                    }
+                }
+            }
 
-			recordRepository.saveAll(records);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+            recordRepository.saveAll(records);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
-	}
+    }
 
-	@Override
-	public List<Record> getAllNews() {
-		return recordRepository.getAllNews();
-	}
+    @Override
+    public List<Record> getAllNews() {
+        return recordRepository.getAllNews();
+    }
 
-	@Override
-	public List<Record> getAllNewsByDate(Date fromDate, Date toDate) {
-		return recordRepository.getAllNewsByDate(fromDate, toDate);
-	}
+    @Override
+    public List<Record> getAllNewsByDate(Date fromDate, Date toDate) {
+        return recordRepository.getAllNewsByDate(fromDate, toDate);
+    }
 
-	@Override
-	public List<Record> getAllNewsByContentAndDate(String content, Date fromDate, Date toDate) {
+    @Override
+    public List<Record> getAllNewsByContentAndDate(String content, Date fromDate, Date toDate) {
 
-		return recordRepository.getAllNewsByContentAndDate(content, fromDate, toDate);
-	}
+        return recordRepository.getAllNewsByContentAndDate(content, fromDate, toDate);
+    }
 
-	@Override
-	public List<Record> getAllNewsByContent(String content) {
+    @Override
+    public List<Record> getAllNewsByContent(String content) {
 
-		return recordRepository.getAllNewsByContent(content);
-	}
+        return recordRepository.getAllNewsByContent(content);
+    }
 
 }
